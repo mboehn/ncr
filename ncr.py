8c906f74beb6d310abc02fe3683c39e131c0eaec
Mathias Bøhn Grytemark <mathias@grytemark.no>
2021-08-22 15:45:50 +0200

    python3-fixing

--- ncr.py
+++ ncr.py
@@ -49,31 +49,34 @@ class ThermalPrinter(object):
     _ESC = chr(27)
     _GS = chr(29)
 
+    def _print(self, data):
+        self.printer.write(data.encode())
+
     # Initializing the printer    
     def __init__(self, serialport=SERIALPORT):
         self.printer = serial.Serial(serialport, self.BAUDRATE, timeout=self.TIMEOUT)
-        self.printer.write(self._ESC) # ESC - command
-        self.printer.write(chr(64)) # @   - initialize
+        self._print(self._ESC) # ESC - command
+        self._print(chr(64)) # @   - initialize
 
     def reset(self):
-        self.printer.write(chr(16))
+        self._print(chr(16))
         
     # Print the configuration page
     def printtest(self):
-        self.printer.write(chr(31))
-        self.printer.write(chr(116))
+        self._print(chr(31))
+        self._print(chr(116))
     
     # Feed n lines
     def linefeed(self, lines=1):
-        self.printer.write(chr(20))
-        self.printer.write(chr(lines))
+        self._print(chr(20))
+        self._print(chr(lines))
 
     def fullcut(self):
-        self.printer.write(chr(25))
+        self._print(chr(25))
 
     # No difference from full cut.
     def partialcut(self):
-        self.printer.write(chr(26))
+        self._print(chr(26))
 
     def justify(self, align="L"):
         pos = 0
@@ -83,74 +86,74 @@ class ThermalPrinter(object):
             pos = 1
         elif align == "R":
             pos = 2
-        self.printer.write(self._ESC)
-        self.printer.write(chr(97))
-        self.printer.write(chr(pos))
+        self._print(self._ESC)
+        self._print(chr(97))
+        self._print(chr(pos))
 
     def bold_off(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(69))
-        self.printer.write(chr(0))
+        self._print(self._ESC)
+        self._print(chr(69))
+        self._print(chr(0))
 
     def bold_on(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(69))
-        self.printer.write(chr(1))
+        self._print(self._ESC)
+        self._print(chr(69))
+        self._print(chr(1))
 
     def font_b_off(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(33))
-        self.printer.write(chr(0))
+        self._print(self._ESC)
+        self._print(chr(33))
+        self._print(chr(0))
 
     def font_b_on(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(33))
-        self.printer.write(chr(1))
+        self._print(self._ESC)
+        self._print(chr(33))
+        self._print(chr(1))
 
     def underline_off(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(45))
-        self.printer.write(chr(0))
+        self._print(self._ESC)
+        self._print(chr(45))
+        self._print(chr(0))
 
     def underline_on(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(45))
-        self.printer.write(chr(1))
+        self._print(self._ESC)
+        self._print(chr(45))
+        self._print(chr(1))
 
     def inverse_off(self):
-        self.printer.write(chr(29))
-        self.printer.write(chr(66))
-        self.printer.write(chr(0))
+        self._print(chr(29))
+        self._print(chr(66))
+        self._print(chr(0))
 
     def inverse_on(self):
-        self.printer.write(chr(29))
-        self.printer.write(chr(66))
-        self.printer.write(chr(1))
+        self._print(chr(29))
+        self._print(chr(66))
+        self._print(chr(1))
 
     def upsidedown_off(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(123))
-        self.printer.write(chr(0))
+        self._print(self._ESC)
+        self._print(chr(123))
+        self._print(chr(0))
 
     def upsidedown_on(self):
-        self.printer.write(self._ESC)
-        self.printer.write(chr(123))
-        self.printer.write(chr(1))
+        self._print(self._ESC)
+        self._print(chr(123))
+        self._print(chr(1))
         
     def barcode_chr(self, msg):
-        self.printer.write(chr(29)) # Leave
-        self.printer.write(chr(72)) # Leave
-        self.printer.write(msg)     # Print barcode # 1:Abovebarcode 2:Below 3:Both 0:Not printed
+        self._print(chr(29)) # Leave
+        self._print(chr(72)) # Leave
+        self._print(msg)     # Print barcode # 1:Abovebarcode 2:Below 3:Both 0:Not printed
         
     def barcode_height(self, msg):
-        self.printer.write(chr(29))  # Leave
-        self.printer.write(chr(104)) # Leave
-        self.printer.write(msg)      # Value 1-255 Default 50
+        self._print(chr(29))  # Leave
+        self._print(chr(104)) # Leave
+        self._print(msg)      # Value 1-255 Default 50
         
     def barcode_height(self):
-        self.printer.write(chr(29))  # Leave
-        self.printer.write(chr(119)) # Leave
-        self.printer.write(chr(2))   # Value 2,3 Default 2
+        self._print(chr(29))  # Leave
+        self._print(chr(119)) # Leave
+        self._print(chr(2))   # Value 2,3 Default 2
         
     def barcode(self, msg):
         """ Please read http://www.adafruit.com/datasheets/A2-user%20manual.pdf
@@ -162,25 +165,25 @@ class ThermalPrinter(object):
         # 68=EAN8    7,8    #74=CODE11    >1
         # 69=CODE39    >1    #75=MSI        >1
         # 70=I25        >1 EVEN NUMBER           
-        self.printer.write(chr(29))  # LEAVE
-        self.printer.write(chr(107)) # LEAVE
-        self.printer.write(chr(65))  # USE ABOVE CHART
-        self.printer.write(chr(12))  # USE CHART NUMBER OF CHAR 
-        self.printer.write(msg)
+        self._print(chr(29))  # LEAVE
+        self._print(chr(107)) # LEAVE
+        self._print(chr(65))  # USE ABOVE CHART
+        self._print(chr(12))  # USE CHART NUMBER OF CHAR 
+        self._print(msg)
         
     def print_text(self, msg, chars_per_line=None):
         """ Print some text defined by msg. If chars_per_line is defined, 
             inserts newlines after the given amount. Use normal '\n' line breaks for 
             empty lines. """ 
         if chars_per_line == None:
-            self.printer.write(msg)
+            self._print(msg)
         else:
             l = list(msg)
             le = len(msg)
             for i in xrange(chars_per_line + 1, le, chars_per_line + 1):
                 l.insert(i, '\n')
-            self.printer.write("".join(l))
-            print "".join(l)
+            self._print("".join(l))
+            print("".join(l))
 
     def print_markup(self, markup):
         """ Print text with markup for styling.
@@ -225,37 +228,37 @@ class ThermalPrinter(object):
             width is enforced to 384 and padded with white if needed. """
         black_and_white_pixels = [1] * 384 * h
         if w > 384:
-            print "Bitmap width too large: %s. Needs to be under 384" % w
+            print("Bitmap width too large: %s. Needs to be under 384" % w)
             return False
         elif w < 384:
-            print "Bitmap under 384 (%s), padding the rest with white" % w
+            print("Bitmap under 384 (%s), padding the rest with white" % w)
 
-        print "Bitmap size", w
+        print("Bitmap size", w)
 
         if type(pixels[0]) == int: # single channel
-            print " => single channel"
+            print(" => single channel")
             for i, p in enumerate(pixels):
                 if p < self.black_threshold:
                     black_and_white_pixels[i % w + i / w * 384] = 0
                 else:
                     black_and_white_pixels[i % w + i / w * 384] = 1
         elif type(pixels[0]) in (list, tuple) and len(pixels[0]) == 3: # RGB
-            print " => RGB channel"
+            print(" => RGB channel")
             for i, p in enumerate(pixels):
                 if sum(p[0:2]) / 3.0 < self.black_threshold:
                     black_and_white_pixels[i % w + i / w * 384] = 0
                 else:
                     black_and_white_pixels[i % w + i / w * 384] = 1
         elif type(pixels[0]) in (list, tuple) and len(pixels[0]) == 4: # RGBA
-            print " => RGBA channel"
+            print(" => RGBA channel")
             for i, p in enumerate(pixels):
                 if sum(p[0:2]) / 3.0 < self.black_threshold and p[3] > self.alpha_threshold:
                     black_and_white_pixels[i % w + i / w * 384] = 0
                 else:
                     black_and_white_pixels[i % w + i / w * 384] = 1
         else:
-            print "Unsupported pixels array type. Please send plain list (single channel, RGB or RGBA)"
-            print "Type pixels[0]", type(pixels[0]), "haz", pixels[0]
+            print("Unsupported pixels array type. Please send plain list (single channel, RGB or RGBA)")
+            print("Type pixels[0]", type(pixels[0]), "haz", pixels[0])
             return False
 
         return black_and_white_pixels
@@ -315,10 +318,10 @@ class ThermalPrinter(object):
         # might be better to send while printing when dealing with 
         # very large arrays...
         for b in print_bytes:
-            self.printer.write(chr(b))   
+            self._print(chr(b))   
         
         if output_png:
             test_print = open('print-output.png', 'wb')
             test_img.save(test_print, 'PNG')
-            print "output saved to %s" % test_print.name
+            print("output saved to %s" % test_print.name)
             test_print.close()
